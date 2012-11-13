563ed0086037a11976d8379aefb6438681884a84
Valentin Pistol <pistol@cs.duke.edu>
2012-11-13 14:09:34 -0500

    Added support for taint targets. Selectively taint a particular app by package name using /data/taint-targets

--- dalvik/src/main/java/dalvik/system/Zygote.java
+++ dalvik/src/main/java/dalvik/system/Zygote.java
@@ -107,20 +107,21 @@ public class Zygote {
      * dimension having a length of 3 and representing
      * (resource, rlim_cur, rlim_max). These are set via the posix
      * setrlimit(2) call.
+     * @param taintTarget true if implicit flow tracking is enabled for this VM
      *
      * @return 0 if this is the child, pid of the child
      * if this is the parent, or -1 on error.
      */
     public static int forkAndSpecialize(int uid, int gid, int[] gids,
-            int debugFlags, int[][] rlimits) {
+            int debugFlags, int[][] rlimits, boolean taintTarget) {
         preFork();
-        int pid = nativeForkAndSpecialize(uid, gid, gids, debugFlags, rlimits);
+        int pid = nativeForkAndSpecialize(uid, gid, gids, debugFlags, rlimits, taintTarget);
         postFork();
         return pid;
     }
 
     native public static int nativeForkAndSpecialize(int uid, int gid,
-            int[] gids, int debugFlags, int[][] rlimits);
+            int[] gids, int debugFlags, int[][] rlimits, boolean taintTarget);
 
     /**
      * Forks a new VM instance.
@@ -128,9 +129,9 @@ public class Zygote {
      */
     @Deprecated
     public static int forkAndSpecialize(int uid, int gid, int[] gids,
-            boolean enableDebugger, int[][] rlimits) {
+            boolean enableDebugger, int[][] rlimits, boolean taintTarget) {
         int debugFlags = enableDebugger ? DEBUG_ENABLE_DEBUGGER : 0;
-        return forkAndSpecialize(uid, gid, gids, debugFlags, rlimits);
+        return forkAndSpecialize(uid, gid, gids, debugFlags, rlimits, taintTarget);
     }
 
     /**
@@ -161,8 +162,7 @@ public class Zygote {
             long permittedCapabilities, long effectiveCapabilities) {
         preFork();
         int pid = nativeForkSystemServer(uid, gid, gids, debugFlags, rlimits,
-                                         permittedCapabilities,
-                                         effectiveCapabilities);
+                                         permittedCapabilities,                                         effectiveCapabilities);
         postFork();
         return pid;
     }
@@ -173,9 +173,9 @@ public class Zygote {
      */
     @Deprecated
     public static int forkSystemServer(int uid, int gid, int[] gids,
-            boolean enableDebugger, int[][] rlimits) {
+            boolean enableDebugger, int[][] rlimits, boolean taintTarget) {
         int debugFlags = enableDebugger ? DEBUG_ENABLE_DEBUGGER : 0;
-        return forkAndSpecialize(uid, gid, gids, debugFlags, rlimits);
+        return forkAndSpecialize(uid, gid, gids, debugFlags, rlimits, false);
     }
 
     native public static int nativeForkSystemServer(int uid, int gid,
